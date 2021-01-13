440cc3b757cd3e22df7526cddb28519fea18e0a9
Knu753n <knutsen.b@gmail.com>
2021-01-13 08:42:36 +0100

    Added print board feature

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ import random, time, word_list
 
 def select_word():
 	x = word_list.list_of_words[random.randint(1,len(word_list.list_of_words))]
-	print(x)
-select_word()
+	return x
+
+def game_board(word):
+	word_line = ""
+	for each in word:
+		if each == " ":
+			word_line += ' '
+		else:
+			word_line += '-'
+	return word_line
+
+print(game_board("test ord"))
