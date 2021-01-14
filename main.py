6bbb048d0053ba51d2280e7692ac221f308cc9e2
Knu753n <knutsen.b@gmail.com>
2021-01-14 12:37:30 +0100

    **CODE BLOCK** Added letter_index function

--- main.py
+++ main.py
@@ -14,9 +14,22 @@ def game_board(word):
 	return word_line
 
 def check_letter(letter_guess, word):
-	return if letter_guess in word
+	return letter_guess in word
 
+def letter_index(x,y):
+	if check_letter(x,y.lower()):
+		index = 0
+		list_of_indexes = []
+		for each in y:
+			if each == x:
+				list_of_indexes.append(index)
+			index += 1
+		return list_of_indexes
 
 
 # Testing
-print(game_board(str(select_word())))
+random_word = select_word()
+game_board = game_board(random_word)
+print(random_word)
+print(game_board)
+x = input("Skriv en bokstav: ").lower()
